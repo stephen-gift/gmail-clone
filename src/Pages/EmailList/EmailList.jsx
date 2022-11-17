abd7932968c6f9ca1456118a48b4d3598289ad1f
Stephen <stephengift43@gmail.com>
2022-11-17 01:24:55 +0100

    EmailList section

--- src/Pages/EmailList/EmailList.jsx
+++ src/Pages/EmailList/EmailList.jsx
@@ -2,12 +2,16 @@ import {
   ArrowDropDown,
   ChevronLeft,
   ChevronRight,
+  Inbox,
+  LocalOffer,
   MoreVert,
+  People,
   Redo,
   Settings,
 } from "@mui/icons-material";
 import { Checkbox, IconButton } from "@mui/material";
 import React from "react";
+import Section from "../../Component/Section/Section";
 import "./EmailList.css";
 
 const EmailList = () => {
@@ -39,7 +43,9 @@ const EmailList = () => {
         </div>
       </div>
       <div className="emailListSections">
-         
+        <Section Icon={<Inbox />} title="primary" color="red" selected={true} />
+        <Section Icon={<People />} title="social" color="#1A73E8" />
+        <Section Icon={<LocalOffer />} title="promotions" color="green" />
       </div>
     </div>
   );
